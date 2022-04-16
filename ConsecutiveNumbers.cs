731012b8d23119acbefa29f3ea3a6ce7d78fe01e
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-16 11:09:14 -0500

    created consecutive numbers class

---
+++ ConsecutiveNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsExercise
+{
+    public class ConsecutiveNumbers
+    {
+        public static string HasConsecutiveNums(int[] nums)
+        {
+            Array.Sort(nums);
+            bool isConsecutive = true;
+            for (var i = 0; i < nums.Count() - 1; i++)
+            {
+                if ((nums[i] + 1) != nums[i + 1])
+                {
+                    isConsecutive = false;
+                    return "Not Consecutive";
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (isConsecutive)
+            {
+                return "Consecutive";
+            }
+            return "Consecutive";
+        } 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,45 @@ namespace StringsExercise
     {
         static void Main(string[] args)
         {
+            //1 - Write a program and ask the user to enter a few numbers 
+            //    separated by a hyphen. Work out if the numbers are consecutive.
+            //    For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
+            //    display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            var list2 = new int[] { 1, 3, 5, 7, 9 };
+            var list3 = new int[] { 12, 11, 10, 9, 8 };
+            Console.WriteLine(ConsecutiveNumbers.HasConsecutiveNums(list));
+            Console.WriteLine(ConsecutiveNumbers.HasConsecutiveNums(list2));
+            Console.WriteLine(ConsecutiveNumbers.HasConsecutiveNums(list3));
+
+            //list3.Sort();
+            //bool isConsecutive = true;
+            //for (var i = 0; i < list3.Count(); i++)
+            //{
+            //    if (i == (list3.Count() - 1))
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        if ((list3[i] + 1) != list3[i + 1])
+            //        {
+            //            Console.WriteLine("not consecutive");
+            //            isConsecutive = false;
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            continue;
+            //        }
+            //    }
+            //}
+            //if (isConsecutive)
+            //{
+            //    Console.WriteLine("Consecutive");
+            //}
+
         }
     }
 }
