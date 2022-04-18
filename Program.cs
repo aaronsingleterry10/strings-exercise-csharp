e0d3f17c8b2da86cf500f7bf9d0efe5bfc54b766
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-17 21:55:23 -0500

    added time class

--- Program.cs
+++ Program.cs
@@ -31,16 +31,38 @@ namespace StringsExercise
             //    by a hyphen. If the user simply presses Enter, without supplying an 
             //    input, exit immediately; otherwise, check to see if there are duplicates. 
             //    If so, display "Duplicate" on the console.
-            Console.Write("Enter in a few numbers separated by a hyphen: ");
-            var userInput = Console.ReadLine();
-            if (!String.IsNullOrWhiteSpace(userInput))
-            {
-                Console.WriteLine(Numbers.DuplicateNums(userInput));
-            }
-            //while (!String.IsNullOrWhiteSpace(userInput))
+            //Console.Write("Enter in a few numbers separated by a hyphen: ");
+            //var userInput = Console.ReadLine();
+            //if (!String.IsNullOrWhiteSpace(userInput))
             //{
-            //    Numbers.DuplicateNums(userInput);
+            //    Console.WriteLine(Numbers.DuplicateNums(userInput));
             //}
+
+            /* 
+             3- Write a program and ask the user to enter a time value in the 24-hour 
+            time format (e.g. 19:00). A valid time should be between 00:00 and 23:59. 
+            If the time is valid, display "Ok"; otherwise, display "Invalid Time". 
+            If the user doesn't provide any values, consider it as invalid time.
+            */
+
+            var input = "25:00";
+            var newInput = TimeSpan.Parse(input);
+            var startTime = new TimeSpan(0, 0, 0);
+            var endTime = new TimeSpan(23, 59, 0);
+            var output = startTime <= newInput && endTime >= newInput;
+            Console.WriteLine(output);
+
+            /*
+            4- Write a program and ask the user to enter a few words separated by a space. 
+            Use the words to create a variable name with PascalCase. For example, if the user types: "number of students", display "NumberOfStudents". Make sure that the program is not dependent on the input. 
+            So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+            */
+
+            /*
+            5- Write a program and ask the user to enter an English word. Count the number 
+            of vowels (a, e, o, u, i) in the word. So, if the user enters "inadequate", 
+            the program should display 6 on the console.
+            */
         }
     }
 }
